5b4176e40d57210443522147bbbc5b1137d64b41
unknown <chandral.thakor@gmail.com>
2020-01-16 23:52:44 +0530

    Add link method

--- linked-list.py
+++ linked-list.py
@@ -15,11 +15,26 @@ class LinkedList(object):
         self.head = None
         self.size = 0
 
-    def add_link(self):
-        current_node = self.head
+    def add_link(self, link):
+        if self.head:
+            current_link = self.head
+            while current_link.next_node:
+                current_link = current_link.next_node
+            current_link.next_node = link
+        else:
+            self.head = link
 
     def remove_link_at(self, position):
         pass
 
     def get_data_at(self, position):
         pass
+
+
+ll = LinkedList()
+n1 = Link(1)
+n2 = Link(2)
+n3 = Link(3)
+ll.add_link(n1)
+ll.add_link(n2)
+ll.add_link(n3)
