e46e07fcdc7c5553155958c040011056a4a5858b
Chandral Thakor <chandral.thakor@gmail.com>
2020-01-17 16:23:17 +0530

    Added more methods

--- linked-list.py
+++ linked-list.py
@@ -28,21 +28,44 @@ class LinkedList(object):
         else:
             print("Please provide a Link object")
 
-    def insert_link_at(self, position):
-        try:
-            position = int(position)
-        except ValueError:
-            print("Please provide an integer")
+    def insert_link_at(self, position, link):
+        if isinstance(link, Link):
+            try:
+                position = int(position)
+                if 0 < position <= self.size:
+                    preceding_node = self.head
+                    for pos in range(0, position-1):
+                        preceding_node = preceding_node.next_node
+                    print(preceding_node.data)
+                else:
+                    print("No existing link at this position. Try add_link() method")
+            except ValueError:
+                print("Please provide an integer")
+        else:
+            print("Please provide a Link object")
 
     def remove_link_at(self, position):
         try:
             position = int(position)
+            if 0 < position <= self.size:
+                preceding_node = self.head
+                for pos in range(0, position - 1):
+                    preceding_node = preceding_node.next_node
+                print(preceding_node.data)
+            else:
+                print("No link found at this position to remove")
         except ValueError:
             print("Please provide an integer")
 
     def get_data_at(self, position):
         try:
             position = int(position)
+            if 0 < position <= self.size:
+                preceding_node = self.head
+                for pos in range(0, position - 1):
+                    preceding_node = preceding_node.next_node
+                return preceding_node.data
+            return "No link found at this position"
         except ValueError:
             print("Please provide an integer")
 
@@ -57,8 +80,10 @@ ll = LinkedList()
 n1 = Link(1)
 n2 = Link(2)
 n3 = Link(3)
+n4 = Link(4)
+
 ll.add_link(n1)
 ll.add_link(n2)
 ll.add_link(n3)
-ll.add_link("asdfasd")
 
+ll.insert_link_at(3, n4)
