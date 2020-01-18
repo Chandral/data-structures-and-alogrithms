dc1183a80c3d3c2375a51850fb4c6dd4df11708c
Chandral Thakor <chandral.thakor@gmail.com>
2020-01-18 18:05:08 +0530

    Solved queues

--- queues.py
+++ queues.py
@@ -16,7 +16,8 @@ class Queue:
         return self.storage[0]
 
     def dequeue(self):
-        return self.storage.pop()
+        return self.storage.pop(0)
+
 
 # Setup
 q = Queue(1)
@@ -25,20 +26,20 @@ q.enqueue(3)
 
 # Test peek
 # Should be 1
-print(q.peek())
+print(q.peek() == 1)
 
 # Test dequeue
 # Should be 1
-print(q.dequeue())
+print(q.dequeue() == 1)
 
 # Test enqueue
 q.enqueue(4)
 # Should be 2
-print(q.dequeue())
+print(q.dequeue() == 2)
 # Should be 3
-print(q.dequeue())
+print(q.dequeue() == 3)
 # Should be 4
-print(q.dequeue())
+print(q.dequeue() == 4)
 q.enqueue(5)
 # Should be 5
-print(q.peek())
+print(q.peek() == 5)
