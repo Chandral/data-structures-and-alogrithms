9d3cf8d5e370aca512377a96ca1a9ec93070438e
Chandral Thakor <chandral.thakor@gmail.com>
2020-02-14 12:53:36 +0530

    Searches and traverses through the tree

--- code/binary-tree.py
+++ code/binary-tree.py
@@ -13,22 +13,31 @@ class BinaryTree(object):
         """Return True if the value
         is in the tree, return
         False otherwise."""
-        return False
+        return self.preorder_search(self.root, find_val)
 
     def print_tree(self):
         """Print out all tree nodes
         as they are visited in
         a pre-order traversal."""
-        return ""
+        return self.preorder_print(self.root, "")[:-1]
 
     def preorder_search(self, start, find_val):
         """Helper method - use this to create a
         recursive search solution."""
+        if start:
+            if start.value == find_val:
+                return True
+            else:
+                return self.preorder_search(start.left, find_val) or self.preorder_search(start.right, find_val)
         return False
 
     def preorder_print(self, start, traversal):
         """Helper method - use this to create a
         recursive print solution."""
+        if start:
+            traversal += str(start.value) + '-'
+            traversal = self.preorder_print(start.left, traversal)
+            traversal = self.preorder_print(start.right, traversal)
         return traversal
 
 
@@ -38,6 +47,8 @@ tree.root.left = Node(2)
 tree.root.right = Node(3)
 tree.root.left.left = Node(4)
 tree.root.left.right = Node(5)
+# tree.root.right.left = Node(6)
+# tree.root.right.right = Node(7)
 
 # Test search
 # Should be True
@@ -48,3 +59,4 @@ print(tree.search(6))
 # Test print_tree
 # Should be 1-2-4-5-3
 print(tree.print_tree())
+
