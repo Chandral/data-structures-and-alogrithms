474ee6c6dd689973e6530d68dbb059fb998c1b4c
Chandral <chandral.thakor@gmail.com>
2020-05-11 14:10:55 +0530

    Update hashing.py

--- code/hashing.py
+++ code/hashing.py
@@ -1,45 +1,51 @@
+"""Write a HashTable class that stores strings
+in a hash table, where keys are calculated
+using the first two letters of the string."""
+
 class HashTable(object):
     def __init__(self):
         self.table = [None]*10000
 
-    def calculate_hash_value(self, string):
-        value = ord(string[0])*100 + ord(string[1])
-        return value
-
     def store(self, string):
-        hv = self.calculate_hash_value(string)
-        if hv != -1:
-            if self.table[hv]:
-                self.table[hv].append(string)
-            else:
-                self.table[hv] = [string]
+        """Input a string that's stored in 
+        the table."""
+        if self.table[self.calculate_hash_value(string)]:
+            self.table[self.calculate_hash_value(string)].append(string)
+        else:
+            self.table[self.calculate_hash_value(string)] = [string]
 
     def lookup(self, string):
-        hv = self.calculate_hash_value(string)
-        if hv != -1:
-            if self.table[hv]:
-                if string in self.table[hv]:
-                    return hv
-        return -1
+        """Return the hash value if the
+        string is already in the table.
+        Return -1 otherwise."""
+        if self.table[self.calculate_hash_value(string)]:
+            return self.calculate_hash_value(string)
+        else:
+            return -1
 
-
+    def calculate_hash_value(self, string):
+        """Helper function to calulate a
+        hash value from a string."""
+        hash_value = (ord(string[0])* 100) + ord(string[1])
+        return hash_value
+    
 # Setup
 hash_table = HashTable()
 
 # Test calculate_hash_value
 # Should be 8568
-print(hash_table.calculate_hash_value('UDACITY'))
+print hash_table.calculate_hash_value('UDACITY')
 
 # Test lookup edge case
 # Should be -1
-print(hash_table.lookup('UDACITY'))
+print hash_table.lookup('UDACITY')
+
 # Test store
 hash_table.store('UDACITY')
 # Should be 8568
-print(hash_table.lookup('UDACITY'))
+print hash_table.lookup('UDACITY')
+
 # Test store edge case
 hash_table.store('UDACIOUS')
-print(hash_table.table[8568])
-
 # Should be 8568
-print(hash_table.lookup('UDACIOUS'))
+print hash_table.lookup('UDACIOUS')
